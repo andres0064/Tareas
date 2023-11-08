07e5fdbb988d71b771aba466cf6037e65436dd13
andres0064 <22532675+andres0064@users.noreply.github.com>
2023-11-07 22:56:43 -0600

    Agregar MainApp, la clase principal MainApp con solo un println

---
+++ src/main/java/com/andres0064/task/MainApp.java
@@ -0,0 +1,12 @@
+package com.andres0064.task;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Where We are: MainApp main" );
+
+	}
+
+}
